5d2f78ffb6222c37615d995c0bf50be110cfb826
allu94 <alvar.martti@gmail.com>
2015-11-21 17:42:33 +0200

    Set bananas to fly nicer

--- src/BananaGun.cpp
+++ src/BananaGun.cpp
@@ -2,7 +2,7 @@
 #include "Banana.hpp"
 #include <iostream>
 
-BananaGun::BananaGun(Game* game): Weapon(10, 0.5f, 3, 10.0f, game){
+BananaGun::BananaGun(Game* game): Weapon(10, 0.5f, 3, 25.0f, game){
 	//Adding bananas to the stack
 	
 	for(unsigned int i = 0; i<3*clipSize; i++){
@@ -32,7 +32,7 @@ void BananaGun::shoot(float angle, b2Vec2 position, b2Vec2 preSpeed, Game* game)
 	for(auto entity: entities){
 		if(!entity->isAlive() && typeid(*entity) == typeid(Banana)) {
 			entity->setAlive(true);
-			entity->getBody()->SetAngularVelocity(1);//Adding a nice spin
+			entity->getBody()->SetAngularVelocity(25);//Adding a nice spin
 			launchProjectile(angle, position, preSpeed, entity);
 			fireClock.restart();//restarting the fire rate observing clock
 			ammo--;//reducing ammo in the clip by 1.
