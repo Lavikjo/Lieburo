841d7a44ba2ee4be315d37c9af090cf5dfe201d1
Tatoma4 <tatoma4@gmail.com>
2015-12-02 02:19:11 +0200

    Fixed health bar not going to zero if damage was more than there was hp left

--- src/GUI.hpp
+++ src/GUI.hpp
@@ -18,11 +18,11 @@ private:
 
 	void createBar(sf::RectangleShape &bar, sf::Vector2f pos, sf::Vector2f size, size_t outlineThickness);
 	void createText(sf::Text &text, sf::Vector2f pos, size_t outlineThickness);
-
+	int clamp(int n, int lower, int upper);
 	sf::Font statusFont;
 	sf::Text hpText1, ammoText1, jetText1, hpText2, ammoText2, jetText2;
 	sf::RectangleShape healthBar1, healthBar2;
-	
+
 	static const int HP1_TEXT_OFFSET = 99640;
 	static const int AMMO1_TEXT_OFFSET = 99750;
 	static const int JET1_TEXT_OFFSET = 99900;
@@ -33,5 +33,4 @@ private:
 	static const int JET2_TEXT_OFFSET = 100010;
 	static const int HP2_BAR_OFFSET = 100400;
 
-	Game* game;
 };
